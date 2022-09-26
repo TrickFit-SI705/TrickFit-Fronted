3e39803be0400dfe1e40608b130eb0be3d95a010
SantiagoHO <sansebas71@hotmail.com>
2022-09-26 17:52:36 -0500

    feat: plan  buscar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,20 @@ import { CitaComponent } from './page/cita/cita.component';
 import { CitaListarComponent } from './page/cita/cita-listar/cita-listar.component';
 import { SuscripcionComponent } from './page/suscripcion/suscripcion.component';
 import { SuscripcionListarComponent } from './page/suscripcion/suscripcion-listar/suscripcion-listar.component';
+import { AlimentoCreaeditaComponent } from './page/alimento/alimento-creaedita/alimento-creaedita.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import{MatIconModule} from'@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { PlanCreaeditaComponent } from './page/plan/plan-creaedita/plan-creaedita.component';
+import { RecetaCreaeditaComponent } from './page/receta/receta-creaedita/receta-creaedita.component';
+import { EjercicioCreaeditaComponent } from './page/ejercicio/ejercicio-creaedita/ejercicio-creaedita.component';
+import { BancoCreaeditaComponent } from './page/banco/banco-creaedita/banco-creaedita.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BancoDialogoComponent } from './page/banco/banco-listar/banco-dialogo/banco-dialogo.component';
+import { PlanDialogoComponent } from './page/plan/plan-listar/plan-dialogo/plan-dialogo.component';
+import { BancoBuscarComponent } from './page/banco/banco-buscar/banco-buscar.component';
+import { PlanBuscarComponent } from './page/plan/plan-buscar/plan-buscar.component';
 
 @NgModule({
   declarations: [
@@ -49,14 +63,28 @@ import { SuscripcionListarComponent } from './page/suscripcion/suscripcion-lista
     CitaComponent,
     CitaListarComponent,
     SuscripcionComponent,
-    SuscripcionListarComponent
+    SuscripcionListarComponent,
+    AlimentoCreaeditaComponent,
+    PlanCreaeditaComponent,
+    RecetaCreaeditaComponent,
+    EjercicioCreaeditaComponent,
+    BancoCreaeditaComponent,
+    BancoDialogoComponent,
+    PlanDialogoComponent,
+    BancoBuscarComponent,
+    PlanBuscarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
